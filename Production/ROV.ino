355b88d61c76879dcfdd5115f8ba6968db808195
Jasper Rubin <jasper.rubin@gmail.com>
2018-03-01 22:11:22 -0330

    Restructuring of files
    Restructured files to prepare for writing production code.

---
+++ Production/ROV.ino
@@ -0,0 +1,27 @@
+/*
+Copyright 2018, Jasper Rubin
+Written for Holy Trinity High's Underwater Robotics Team
+All rights to the code lie with them.s
+
+This program is written to run on an Arduino Mega or similar with a MAX485 connected
+through the pins listed below, as well as motor controllers, again as listed.
+
+PINOUT:
+
+[TODO]
+
+COMMUNICATION STRUCTURE:
+
+[TODO]
+
+*/
+
+#include <SoftwareSerial.h>
+
+void setup()
+{
+}
+
+void loop()
+{
+}
