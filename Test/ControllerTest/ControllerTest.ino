fa3f5c50d9be8086ae27f19a0a04c39922952fb0
Jasper Rubin <jasper.rubin@gmail.com>
2018-03-05 22:58:43 -0330

    It works now
    Did something to make ControllerTest work.

--- Test/ControllerTest/ControllerTest.ino
+++ Test/ControllerTest/ControllerTest.ino
@@ -11,23 +11,30 @@ USB Usb;
 XBOXUSB Xbox(&Usb);
 
 void setup() {
+  Serial.begin(115200);
+  pinMode(5, OUTPUT);
   if (Usb.Init() == -1) {
+    Serial.print(F("\r\nOSC did not start"));
     while (1); //halt
   }
-  pinMode(5, OUTPUT);
+  Serial.print(F("\r\nXBOX USB Library Started"));
 }
-
 void loop() {
   Usb.Task();
   if (Xbox.Xbox360Connected) {
-    int i = Xbox.getAnalogHat(LeftHatY);
-    i = map(i, -32768, 32768, 0, 255);
-    if(i > 100){
-      Xbox.setLedOn(LED1);
-    }else {
-        Xbox.setAllOff();
+    if (Xbox.getAnalogHat(LeftHatY) > 7500 || Xbox.getAnalogHat(LeftHatY) < -7500) {
+      if (Xbox.getAnalogHat(LeftHatY) > 7500 || Xbox.getAnalogHat(LeftHatY) < -7500) {
+        Serial.print(F("LeftHatY: "));
+        int i = Xbox.getAnalogHat(LeftHatY);
+        i = map(i, -32768, 32768, 0, 255);
+        Serial.print(i);
+        analogWrite(5, i);
+        Serial.print("\t");
+      }else {
+        analogWrite(5, 128);
+      }
+      Serial.println();
     }
-    analogWrite(i, 5);
   }
-  delay(5);
+  delay(1);
 }
