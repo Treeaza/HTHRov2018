b971e42d5e5a0448533f77e640563168bf34c89f
Treeaza <jasper.rubin@gmail.com>
2018-03-08 19:37:17 -0330

    MOAR WORK
    Yeah. Commit messages really do go downhill after a while.

--- Production/Surface/Surface.ino
+++ Production/Surface/Surface.ino
@@ -19,7 +19,19 @@ PINOUT:
 
 COMMUNICATION STRUCTURE:
 
-[TODO]
+  We can only read one byte at a time, so we send down a character represtenting
+  the axis we wish to move in, then the value as a second unsigned byte. Sorting out how to use
+  that information is the ROV Arduino's job.
+
+  AXES:
+
+    X: Forwards and backwards.
+    Y: Left and right.
+    Z: Up and down.
+    R: Rotate left and right.
+    P: Pitch forwards and backwards.
+    C: Open and close claw.
+    G: Enable/disable auto level (should only ever be sent a 0 or a 1.
 
 */
 
@@ -43,6 +55,8 @@ XBOXUSB Xbox(&Usb);
 //Actual serial object.
 SoftwareSerial MAX(MAXRX, MAXTX);
 
+boolean autoLevelOn = true;
+
 void setup(){
   //Set 485 to transmit. We never actually need to recieve, but it's nice to have the option.
   pinMode(MAXControl, OUTPUT);
@@ -55,7 +69,24 @@ void loop(){
   //Does...something with the shield? I've yet to work out exactly what, but it's important.
   Usb.Task();
   if(Xbox.Xbox360Connected) {
+    alignChannel();
     
   }
+  delay(10);
+}
+
+void alignChannel () {
+  //Send a long string of 0s for reciever to check for, ensuring our communications are
+  //properly aligned. Any more than 2 should work.
+  for(int i = 0; i < 5; i++){
+    MAX.write(0);
+    delay(1);
+  }
+}
+
+void sendCommand (char axis, byte value) {
+  //Just exists for convenience really.
+  MAX.write(axis);
+  Max.write(value);
 }
 
